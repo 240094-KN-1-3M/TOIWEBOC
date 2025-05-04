88fb8778ba24cf408ff9b38553a3a2c0562516dd
240094-KN-1-3M <emanonwhoami@gmail.com>
2025-05-04 20:31:29 +0300

    начал 4 лабу

---
+++ Lab4/index.ts
@@ -0,0 +1,39 @@
+import path from "path";
+import express, { Request, Response } from "express";
+import { routeHello, routeAPINames, routeWeather } from "./routes.js";
+
+const server = express();
+const port = 5000;
+
+server.get('/hello', function (_req: Request, res: Response): void {
+    const response = routeHello();
+    res.send(response);
+});
+
+server.get("/api/names", async function (_req: Request, res: Response): Promise<void> {
+    let response = 'Помилка сервера ((';
+    try {
+        response = await routeAPINames();
+    } catch (err) {
+        console.log(err);
+    }
+    res.send(response);
+});
+
+server.get(
+    "/api/weather/:zipcode",
+    function (req: Request, res: Response): void {
+        const response = routeWeather({ zipcode: req.params.zipcode });
+        res.send(response);
+    });
+
+server.get("/components/weather", function (req: Request, res: Response):
+    void {
+    const filePath = path.join(process.cwd(), "public", "weather.html");
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(filePath);
+});
+
+server.listen(port, function (): void {
+    console.log('Listening on ' + port);
+});
